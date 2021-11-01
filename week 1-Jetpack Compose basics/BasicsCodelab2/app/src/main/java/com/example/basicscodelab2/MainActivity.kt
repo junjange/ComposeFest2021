67c92269ca05865f2638b557d4b80168c64debae
JunJangE <wnswkd486@gmail.com>
2021-11-02 03:28:52 +0900

    week1

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/example/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/example/basicscodelab2/MainActivity.kt
@@ -1,38 +1,183 @@
 package com.example.basicscodelab2
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelab2.ui.theme.BasicsCodelab2Theme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelab2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                MyApp()
                 }
             }
         }
     }
+
+
+@Composable
+fun MyApp() {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000){"$it"}){
+
+    // LazyColumn 와 LazyRow 는 안드로이드 뷰에 리사이클 뷰와 비슷합니다.
+    LazyColumn (modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names){ name ->
+            Greeting(name = name)
+        }
+    }
+//    // 테마의 '배경' 색상을 사용하는 표면 컨테이너
+//    Surface(color = MaterialTheme.colors.background){
+//
+//        // Material 구성 요소 androidx.compose.material.Surface는
+//        // 텍스트에 적절한 색상을 선택하는 것과 같이 앱에서 원하는 일반적인 기능을 처리
+//        Greeting("Android")
+//    }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp))
+     {
+        CardContent(name)
+
+    }
+
+
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun CardContent(name: String){
+
+    // remember 보다 rememberSaveable 이 더 많은 데이터를 저장할 수 있다.
+    var expanded by remember{mutableStateOf(false)}
+
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            // 에니메이션 추가
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+
+                // 더 보기" 및 "더 적게 보기"에 대한 내용 설명이 있어야 하며 간단한 if 문으로 추가
+                // 문자열을 하드 코딩하는 것은 나쁜 습관이므로 strings.xml파일 에서 가져와야 한다.
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                 onClick= onContinueClicked
+            ){
+                Text("Continue")
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelab2Theme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelab2Theme {
-        Greeting("Android")
+        Greetings()
     }
 }
